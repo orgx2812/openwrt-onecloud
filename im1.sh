fd02d85224995984f8436232b6c0b8966145446d
lxiaya <lxiaya@163.com>
2024-06-01 09:17:22 +0000

    update kernel-6.6

---
+++ im1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-im1.sh
+# Description: OpenWrt DIY script part 2 (rm Update feeds)
+#
+
+#=================================================
+sudo rm -rf feeds/packages/net/sing-box
+sudo rm -rf feeds/packages/net/chinadns-ng
+#sudo rm -rf feeds/packages/net/v2ray
+#sudo rm -rf feeds/packages/net/v2ray-core
+#sudo rm -rf feeds/packages/net/v2ray-geodata
+#sudo rm -rf feeds/packages/net/xray-core
+sudo rm -rf feeds/luci/applications/luci-app-homeproxy
